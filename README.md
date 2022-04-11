a8abc73151dfbceee91e936e4d57a6323b933328
Julio <julio.f4costa@hotmail.com>
2022-04-11 10:25:47 -0300

    exercicios distancia entre dois pontos

---
+++ README.md
@@ -0,0 +1,5 @@
+___
+## Distância Entre Dois Ponto(1015)  
+# JS / Python
+<https://www.beecrowd.com.br/judge/pt/problems/view/1015>
+___
---
+++ python/distancia_2Pontos.py
@@ -0,0 +1,10 @@
+import math
+
+x1 = float(input("digite um valor para x1: "))
+y1 = float(input("digite um valor para y1: "))
+x2 = float(input("digite um valor para x2: "))
+y2 = float(input("digite um valor para y2: "))
+
+distancia = float(math.sqrt((x2 - x1)**2 + (y2 - y1)**2))
+
+print(f'A distancia é de: {distancia:,.4f}')
